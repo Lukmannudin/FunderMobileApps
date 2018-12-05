a20550a58bf78a211f6114f16a97695fe3f6de6e
Lukmannudin <laodelukmannudinpriatna@gmail.com>
2018-12-06 03:28:15 +0700

    Login with full of bug

--- app/src/main/java/com/example/unikomcodelabs/funder/MainActivity.kt
+++ app/src/main/java/com/example/unikomcodelabs/funder/MainActivity.kt
@@ -2,11 +2,14 @@ package com.example.unikomcodelabs.funder
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.unikomcodelabs.funder.Login.LoginActivity
+import org.jetbrains.anko.startActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        startActivity<LoginActivity>()
     }
 }
