7e31ee07d22a3cb04af4d6d4597a16e3b1b2f981
Diki Supriadi <dikisupriadi021@email.unikom.ac.id>
2019-01-16 17:29:29 +0700

    xml only

---
+++ app/src/main/java/com/team/oleg/funder/diki.kt
@@ -0,0 +1,12 @@
+package com.team.oleg.funder
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class diki : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.view_company_profile)
+    }
+}
